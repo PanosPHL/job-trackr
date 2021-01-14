56c415edf25b88bfad63f04e368c3c4a8499793f
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-14 15:43:24 -0500

    Set up MUI theme provider and Apollo client

---
+++ client/src/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#424242',
+    },
+    secondary: {
+      main: '#486581',
+    },
+  },
+});
+
+export default theme;
