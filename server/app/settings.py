5a3a537469147b07b51138720fd388102562c1b8
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-14 13:13:06 -0500

    Able to create simple query for fake oauth user

--- server/app/settings.py
+++ server/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "job-trackr",
+    "job_trackr",
     "graphene_django",
 ]
 
