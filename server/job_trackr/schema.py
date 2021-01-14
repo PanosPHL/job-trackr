0b43425adeb0dfd4a1023d355cbcdc5a454588ea
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-14 13:52:02 -0500

    Added query for single user

--- server/job_trackr/schema.py
+++ server/job_trackr/schema.py
@@ -9,8 +9,12 @@ class OAuthUsersType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
   all_users = graphene.List(OAuthUsersType)
+  user = graphene.Field(OAuthUsersType, id=graphene.String(), site=graphene.String())
 
   def resolve_all_users(root, info):
     return OAuthUser.objects.all()
+
+  def resolve_user(root, info, id, site):
+    return OAuthUser.objects.get(id=id, site=site)
 
 schema = graphene.Schema(query=Query)
