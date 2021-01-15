8003fd38f96ed266ac1fcf63e9b1b899fa511950
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-15 14:57:51 -0500

    Server returns OAuth keys on request to GraqpQL API

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
+import { useQuery, gql } from '@apollo/client';
 
-const Login: React.FC<unknown> = () => (
-  <Container>
-    <Paper></Paper>
-  </Container>
-);
+const OAUTH_KEYS = gql`
+  query GetOAuthKeys {
+    all_oauth_keys {
+      githubClientId
+      githubClientSecret
+    }
+  }
+`;
+const Login: React.FC<unknown> = () => {
+  const { loading, error, data } = useQuery(OAUTH_KEYS);
+  return (
+    <Container>
+      <Paper></Paper>
+    </Container>
+  );
+};
 
 export default Login;
--- server/job_trackr/schema.py
+++ server/job_trackr/schema.py
@@ -1,3 +1,4 @@
+import os
 import graphene
 from graphene_django import DjangoObjectType
 from .models import OAuthUser
@@ -7,14 +8,28 @@ class OAuthUsersType(DjangoObjectType):
     model = OAuthUser
     fields = ("id", "first_name", "last_name", "email", "site")
 
+class OAuthKeys(graphene.ObjectType):
+  class Meta:
+    description = "OAuth Keys"
+
+  githubClientId = graphene.String()
+  githubClientSecret = graphene.String()
+
 class Query(graphene.ObjectType):
   all_users = graphene.List(OAuthUsersType)
   user = graphene.Field(OAuthUsersType, id=graphene.String(), site=graphene.String())
+  all_oauth_keys = graphene.Field(OAuthKeys)
 
   def resolve_all_users(root, info):
     return OAuthUser.objects.all()
 
   def resolve_user(root, info, id, site):
     return OAuthUser.objects.get(id=id, site=site)
+
+  def resolve_all_oauth_keys(root, info):
+    return {
+      "githubClientId": os.environ.get("GITHUB_CLIENT_ID"),
+      "githubClientSecret": os.environ.get("GITHUB_CLIENT_SECRET")
+    }
 
 schema = graphene.Schema(query=Query)
